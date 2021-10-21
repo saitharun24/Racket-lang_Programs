c936c4c021ae37e47b0db2e7a5cd9f50e2a473cc
A. Sai Tharun <50414959+saitharun24@users.noreply.github.com>
2021-10-22 01:25:35 +0530

    Update Sudoku_Solver.rkt

--- Sudoku_Solver/Sudoku_Solver.rkt
+++ Sudoku_Solver/Sudoku_Solver.rkt
@@ -30,24 +30,24 @@
   
   ;Function to check if the number being placed in the particular cell is valid 
   (define (check_num? b i r c)
-    (or (member i (list-ref b r))                                             ;Check if the number is already in the same row
-        (member i (map (lambda(x)(list-ref x c)) b))                          ;Check if the number is already in the same column
-        (member i (subgrid b r c))))                                          ;Check if the number is already in the grid
+    (or (member i (list-ref b r))                                               ;Check if the number is already in the same row
+        (member i (map (lambda(x)(list-ref x c)) b))                            ;Check if the number is already in the same column
+        (member i (subgrid b r c))))                                            ;Check if the number is already in the grid
 
   
   ;Main procedure where the backtracking happens
   (let loop ((b board) (row 0) (col 0))
     (let ((next (lambda(block)
-                  (cond [(< col 8) (loop block row (add1 col))]               ;Iterating the columns one by one 
-                        [(< row 8) (loop block (add1 row) 0)]                 ;Iterating the rows one by one
-                        [else (print_board block "Solution obtained:")]))))   ;Printing the board once every cell has been traversed
-      (if (= 0 (get_num b row col))                                           ;Check if the cell is empty
-          (begin (for ((i (range 1 10)))                                      ;If the cell is empty then place values from 1 till 9 while checking whether the particular value can be placed in the cell 
-                   (unless(check_num? b i row col)                            ;Checking if the value can be placed
-                     (define block                                            ;If yes then placing the value in the particular cell and then go to the next cell
+                  (cond [(< col 8) (loop block row (add1 col))]                 ;Iterating the columns one by one 
+                        [(< row 8) (loop block (add1 row) 0)]                   ;Iterating the rows one by one
+                        [else (print_board block "Solution obtained:")]))))     ;Printing the board once every cell has been traversed
+      (if (= 0 (get_num b row col))                                             ;Check if the cell is empty
+          (begin (for ((i (range 1 10)))                                        ;If the cell is empty then place values from 1 till 9 while checking whether the particular value can be placed in the cell 
+                   (unless(check_num? b i row col)                              ;Checking if the value can be placed
+                     (define block                                              ;If yes then placing the value in the particular cell and then go to the next cell
                        (list-set b row (list-set (list-ref b row) col i)))
                      (next block))))
-          (next b)))))                                                        ;If the cell already has a value then we go to the next cell
+          (next b)))))                                                          ;If the cell already has a value then we go to the next cell
 
 
 
